8b842a81a72d4aa379f1206b1256b5e4a90507a6
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-06 11:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll("input")
+let submit = document.querySelector("#submit")
+
+submit.addEventListener("click",()=>{
+    let body = document.querySelector("book-list")
+    let row = body.insertRow()
+    for(let i=0;i<4;i++){
+        let cell = row.insertCell(i)
+        if(i==3){
+            cell.textContent = "cross"
+        }
+        else{
+            cell.textContent = `${inputs[i].value}`
+        }
+    }
+})
